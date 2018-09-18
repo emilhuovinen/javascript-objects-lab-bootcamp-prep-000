b6ef4cb5a12b461170f4c491758dff6c79858bd6
emil.huo <emil.huo@gmail.com>
2018-09-18 08:50:30 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return Object.assign(object, { [key]: value })
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  return delete object[key];
+}
